c1c9ec8be64840a090f91878d33d0027ab766677
tonyb <tbuelvasyepez@gmail.com>
2024-04-27 20:47:59 -0500

    ultima actualización

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -12,4 +13,8 @@ class Post(models.Model):
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.no
+        self.published_date = timezone.now()
+        self.save()
+
+    def _str_(self):
+        return self.title
